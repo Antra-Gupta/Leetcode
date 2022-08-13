fa625f6f01492a56f53fd0c3505a057dd45d209c
Antra Gupta <90148290+Antra-Gupta@users.noreply.github.com>
2022-08-13 18:40:48 +0530

    Time: 24 ms (69.84%), Space: 12 MB (63.65%) - LeetHub

---
+++ 518-coin-change-2/518-coin-change-2.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int change(int amount, vector<int>& coins) {
+       int n=coins.size();
+        int t[n+1][amount+1];
+        for(int i=0;i<=n;i++){
+            t[i][0]=1;
+        }
+        for(int j=0;j<=amount;j++){
+            t[0][j]=0;
+        }
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=amount;j++){
+                if(coins[i-1]<=j){
+                    t[i][j]=t[i-1][j]+t[i][j-coins[i-1]];
+                }
+                else{
+                    t[i][j]=t[i-1][j];
+                }
+            }
+        }
+        return t[n][amount];
+    }
+};
