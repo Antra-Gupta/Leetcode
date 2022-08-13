8565c24fba7f17e1449f6cb7f9133a3da76b119d
Antra Gupta <90148290+Antra-Gupta@users.noreply.github.com>
2022-08-13 17:56:53 +0530

    Time: 91 ms (85.41%), Space: 10.5 MB (91.01%) - LeetHub

---
+++ 322-coin-change/322-coin-change.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int coinChange(vector<int>& coins, int amount) {
+        int n=coins.size();
+        int t[n+1][amount+1];
+            for(int j=0;j<amount+1;j++){
+                    t[0][j]=INT_MAX-1;
+            }
+            for(int i=0;i<n+1;i++){
+                    t[i][0]=0;
+            }
+        
+        for(int j=1;j<amount+1;j++){
+            if(j%coins[0]==0){
+                t[1][j]=j/coins[0];
+            }
+            else{
+                t[1][j]=INT_MAX-1;
+            }
+        }
+        for(int i=1;i<n+1;i++){
+            for(int j=1;j<amount+1;j++){
+                 if(coins[i-1]<=j){
+            t[i][j]=min(t[i][j-coins[i-1]]+1,t[i-1][j]);
+        }
+        else{
+            t[i][j]=t[i-1][j];
+        }
+            }
+        }
+       
+        return t[n][amount]==INT_MAX-1?-1:t[n][amount];
+    }
+};
