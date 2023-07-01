ecc04e6c85787fcd79c5d1f3582d2dcc9a61d613
Antra Gupta <90148290+Antra-Gupta@users.noreply.github.com>
2023-07-02 00:12:43 +0530

    Added solution - LeetHub

--- Factorials of large numbers - GFG/factorials-of-large-numbers.cpp
+++ Factorials of large numbers - GFG/factorials-of-large-numbers.cpp
@@ -8,20 +8,52 @@ using namespace std;
 
 class Solution {
 public:
+class Node{
+    public:
+    int data;
+    Node *next;
+    Node(int n){
+        data=n;
+        next=NULL;
+    }
+};
+    void multiply(Node* &tail,int n){
+        Node *prev=tail;
+        Node *temp=tail;
+        int carr=0;
+        while(temp!=NULL){
+            int val=temp->data*n+carr;
+            temp->data=val%10;
+            carr=val/10;
+            prev=temp;
+            temp=temp->next;
+        }
+        while(carr!=0){
+            prev->next=new Node((int)carr%10);
+            carr=carr/10;
+            prev=prev->next;
+        }
+    }
     vector<int> factorial(int N){
         vector<int> res;
-        res.push_back(1);
+        Node *tail=new Node(1);
+        // res.push_back(1);
         for(int j=2;j<=N;j++){
-            int carr=0;
-            for(int i=0;i<res.size();i++){
-                int val=res[i]*j+carr;
-                res[i]=val%10;
-                carr=val/10;
-            }
-            while(carr!=0){
-                res.push_back(carr%10);
-                carr=carr/10;
-            }
+            multiply(tail,j);
+            // int carr=0;
+            // for(int i=0;i<res.size();i++){
+            //     int val=res[i]*j+carr;
+            //     res[i]=val%10;
+            //     carr=val/10;
+            // }
+            // while(carr!=0){
+            //     res.push_back(carr%10);
+            //     carr=carr/10;
+            // }
+        }
+        while(tail!=NULL){
+            res.push_back(tail->data);
+            tail=tail->next;
         }
         reverse(res.begin(),res.end());
         return res;
