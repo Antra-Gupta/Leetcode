6abafadcf3b1c92c821c826d233f0bbaf4601aa2
Antra Gupta <90148290+Antra-Gupta@users.noreply.github.com>
2023-08-10 23:33:07 +0530

    Added solution - LeetHub

--- Longest Common Subsequence - GFG/longest-common-subsequence.cpp
+++ Longest Common Subsequence - GFG/longest-common-subsequence.cpp
@@ -15,13 +15,13 @@ class Solution
        int dp[n+1][m+1];
        for(int i=0;i<=n;i++){
            for(int j=0;j<=m;j++){
-               if(i==0||j==0){
+               if(i==0 || j==0){
                    dp[i][j]=0;
                }
                else if(s1[i-1]==s2[j-1]){
                    dp[i][j]=dp[i-1][j-1]+1;
                }
-               else{
+               else {
                    dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
                }
            }
